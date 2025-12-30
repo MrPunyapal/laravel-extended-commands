aa2ed5b39a965b03ecc0f8301d5efb91225725fb
Punyapal Shah <mrpunyapal@gmail.com>
2025-12-30 17:00:11 +0530

    formatting

--- src/Commands/FacadeMakeCommand.php
+++ src/Commands/FacadeMakeCommand.php
@@ -2,152 +2,259 @@
 
 namespace MrPunyapal\LaravelExtendedCommands\Commands;
 
-use Illuminate\Console\Command;
-use Illuminate\Filesystem\Filesystem;
-use Illuminate\Support\Pluralizer;
+use Illuminate\Console\GeneratorCommand;
 use Illuminate\Support\Str;
+use Override;
 use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
 
 #[AsCommand(name: 'make:facade')]
-class FacadeMakeCommand extends Command
+class FacadeMakeCommand extends GeneratorCommand
 {
     /**
-     * The name and signature of the console command.
+     * The console command name.
      *
      * @var string
      */
-    protected $signature = 'make:facade {facadeName?} {facadeServiceClass?}';
+    protected $name = 'make:facade';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'This command is used to create a new facade.';
+    protected $description = 'Create a new facade and service class';
 
     /**
-     * Create a new command instance.
+     * The type of class being generated.
+     *
+     * @var string
      */
-    public function __construct(/**
-     * Filesystem instance
+    protected $type = 'Facade';
+
+    /**
+     * The service class name.
+     *
+     * @var string
      */
-        protected Filesystem $files)
-    {
-        parent::__construct();
-    }
+    protected $serviceClassName;
 
     /**
      * Execute the console command.
      */
-    public function handle(): void
+    #[Override]
+    public function handle()
     {
-        $argument_name_facade = 'facadeName';
-        $facadeName = $this->getArgumentValueAndValidation($argument_name_facade, 'Enter FacadeName (ex. FileUpload)');
+        // Get the service class name
+        $this->serviceClassName = $this->argument('serviceClass');
 
-        $argument_name_facade_service_class = 'facadeServiceClass';
-        $facadeServiceClass = $this->getArgumentValueAndValidation($argument_name_facade_service_class, 'Enter FacadeServiceClass (ex. CommonFileUpload)');
-
-        $facadepath = $this->getValidatePath('Facades/', $facadeName);
-        $facadeServicePath = $this->getValidatePath('Facades/Services/', $facadeServiceClass);
-
-        if ($this->files->exists($facadepath)) {
-            $this->warn("Facade File : {$facadepath} Already Exist");
-            exit();
+        if (empty($this->serviceClassName)) {
+            $this->serviceClassName = $this->ask('Enter FacadeServiceClass (ex. CommonFileUpload)');
         }
 
-        if ($this->files->exists($facadeServicePath)) {
-            $this->warn("Facade Service File : {$facadeServicePath} Already Exist");
-            exit();
+        if (empty($this->serviceClassName)) {
+            $this->error('Service class name is required!');
+
+            return false;
         }
 
-        // Created Directory Facades & its services
-        $this->makeDirectory(dirname($facadepath));
-        $this->makeDirectory(dirname($facadeServicePath));
-
-        /**
-         * Generate Both the Files
-         */
-        $this->generateFacadeClass($facadepath, $facadeServicePath, $facadeName, $facadeServiceClass);
-        $this->generateFacadeServiceClass($facadepath, $facadeServicePath, $facadeName, $facadeServiceClass);
-        $this->info('Facades Created');
-
-        $this->info('AppServiceProvider register the code and start');
-        $this->info('$this->app->singleton("'.Str::lower($facadeServiceClass).'", function ($app) {
-            return app('.$facadeServiceClass.'::class);
-        });');
-    }
-
-    public function getValidatePath(string $path, string $filename): string
-    {
-        return app_path($path.$filename.'.php');
-    }
-
-    protected function getArgumentValueAndValidation($argument_name_facade, $message): string
-    {
-        $name = $this->argument($argument_name_facade);
-
-        while (empty($name)) {
-            $name = $this->ask($message);
-
-            if (empty($name)) {
-                $this->error('This is required Field!');
-            }
+        // Generate facade
+        if (parent::handle() === false && ! $this->option('force')) {
+            return false;
         }
 
-        return $name;
+        // Generate service class
+        $this->createServiceClass($this->serviceClassName);
+
+        $this->displayRegistrationInstructions($this->serviceClassName);
+
+        return null;
     }
 
-    protected function generateFacadeClass($facadepath, $facadeServicePath, $facadeName, $facadeServiceClass)
+    /**
+     * Create the service class for the facade.
+     */
+    protected function createServiceClass(string $name): void
     {
-        $stubsVariable = [
-            'NAMESPACE' => 'App\\Facades',
-            'CLASS_NAME' => ucwords(Pluralizer::singular($facadeName)),
-            'SERVICE_CLASS_NAME' => Str::lower($facadeServiceClass),
+        $path = $this->getServicePath($name);
+
+        if ($this->files->exists($path) && ! $this->option('force')) {
+            $this->components->error("Service class [{$path}] already exists.");
+
+            return;
+        }
+
+        $this->makeDirectory($path);
+
+        $this->files->put($path, $this->buildServiceClass($name));
+
+        $this->components->info(sprintf('Service class [%s] created successfully.', $path));
+    }
+
+    /**
+     * Get the destination path for the service class.
+     */
+    protected function getServicePath(string $name): string
+    {
+        $name = str_replace('\\', '/', $name);
+
+        return $this->laravel->basePath('app/Facades/Services/'.$name.'.php');
+    }
+
+    /**
+     * Build the service class with the given name.
+     */
+    protected function buildServiceClass(string $name): string
+    {
+        $stub = $this->files->get($this->getServiceStub());
+
+        return $this->replaceServiceNamespace($stub, $name)->replaceServiceClass($stub, $name);
+    }
+
+    /**
+     * Replace the namespace for the service class.
+     */
+    protected function replaceServiceNamespace(string &$stub, string $name): static
+    {
+        $searches = [
+            ['DummyNamespace', 'DummyRootNamespace', 'NamespacedDummyUserModel'],
+            ['{{ namespace }}', '{{ rootNamespace }}', '{{ namespacedUserModel }}'],
+            ['{{namespace}}', '{{rootNamespace}}', '{{namespacedUserModel}}'],
         ];
 
-        $facadeStubsPath = __DIR__.'/../../stubs/facades.stub';
+        $namespace = 'App\\Facades\\Services';
 
-        $contents = $this->getStubContents($facadeStubsPath, $stubsVariable);
+        $normalizedName = str_replace('\\', '/', $name);
+        if (str_contains($normalizedName, '/')) {
+            $namespace .= '\\'.str_replace('/', '\\', dirname($normalizedName));
+        }
 
-        $this->files->put($facadepath, $contents);
+        foreach ($searches as $search) {
+            $stub = str_replace(
+                $search,
+                [$namespace, $this->rootNamespace(), $this->userProviderModel()],
+                $stub
+            );
+        }
+
+        return $this;
     }
 
-    protected function generateFacadeServiceClass($facadepath, $facadeServicePath, $facadeName, $facadeServiceClass)
+    /**
+     * Replace the class name for the service class.
+     */
+    protected function replaceServiceClass(string &$stub, string $name): string
     {
-        $stubsVariable = [
-            'NAMESPACE' => 'App\\Facades\\Services',
-            'CLASS_NAME' => ucwords(Pluralizer::singular($facadeServiceClass)),
+        $class = str_replace('/', '\\', $name);
+        $class = class_basename($class);
+
+        return str_replace(['DummyClass', '{{ class }}', '{{class}}'], $class, $stub);
+    }
+
+    /**
+     * Get the stub file for the service class.
+     */
+    protected function getServiceStub(): string
+    {
+        return $this->resolveStubPath('/../../stubs/facades-services.stub');
+    }
+
+    /**
+     * Build the class with the given name.
+     */
+    #[Override]
+    protected function buildClass($name)
+    {
+        $stub = parent::buildClass($name);
+
+        return $this->replaceServiceAccessor($stub, $this->serviceClassName);
+    }
+
+    /**
+     * Replace the service accessor in the stub.
+     */
+    protected function replaceServiceAccessor(string $stub, string $serviceClassName): string
+    {
+        $accessor = Str::lower(class_basename(str_replace('/', '\\', $serviceClassName)));
+
+        return str_replace(['{{ serviceAccessor }}', '{{serviceAccessor}}'], $accessor, $stub);
+    }
+
+    /**
+     * Get the stub file for the generator.
+     */
+    #[Override]
+    protected function getStub()
+    {
+        return $this->resolveStubPath('/../../stubs/facades.stub');
+    }
+
+    /**
+     * Resolve the fully-qualified path to the stub.
+     */
+    protected function resolveStubPath(string $stub): string
+    {
+        return file_exists($customPath = $this->laravel->basePath(trim($stub, '/')))
+            ? $customPath
+            : __DIR__.$stub;
+    }
+
+    /**
+     * Get the default namespace for the class.
+     */
+    #[Override]
+    protected function getDefaultNamespace($rootNamespace)
+    {
+        return $rootNamespace.'\\Facades';
+    }
+
+    /**
+     * Display registration instructions for the service provider.
+     */
+    protected function displayRegistrationInstructions(string $serviceClassName): void
+    {
+        $accessor = Str::lower(class_basename(str_replace('/', '\\', $serviceClassName)));
+        $serviceClass = str_replace('/', '\\', $serviceClassName);
+
+        $this->components->info('Register the facade in your AppServiceProvider:');
+        $this->line('');
+        $this->line('$this->app->singleton("'.$accessor.'", function ($app) {');
+        $this->line('    return new \\App\\Facades\\Services\\'.$serviceClass.'();');
+        $this->line('});');
+    }
+
+    /**
+     * Get the console command arguments.
+     */
+    #[Override]
+    protected function getArguments()
+    {
+        return [
+            ['name', InputArgument::OPTIONAL, 'The name of the facade'],
+            ['serviceClass', InputArgument::OPTIONAL, 'The name of the service class'],
         ];
-
-        $facadeStubsPath = __DIR__.'/../../stubs/facades-services.stub';
-
-        $contents = $this->getStubContents($facadeStubsPath, $stubsVariable);
-
-        $this->files->put($facadeServicePath, $contents);
     }
 
-    public function getStubContents($stub, $stubVariables = []): string|array|false
+    /**
+     * Get the console command options.
+     */
+    #[Override]
+    protected function getOptions()
     {
-        $contents = file_get_contents($stub);
-
-        foreach ($stubVariables as $search => $replace) {
-            $contents = str_replace('$'.$search.'$', $replace, $contents);
-        }
-
-        return $contents;
+        return [
+            ['force', 'f', InputOption::VALUE_NONE, 'Create the class even if the facade already exists'],
+        ];
     }
 
-    protected function makeDirectory($path): string
+    /**
+     * Prompt for missing input arguments using the returned questions.
+     */
+    #[Override]
+    protected function promptForMissingArgumentsUsing()
     {
-        if (! $this->files->isDirectory($path)) {
-            $this->files->makeDirectory($path, 0777, true, true);
-        }
-
-        return $path;
-    }
-
-    public function getSignature(): string
-    {
-        return $this->signature;
+        return [
+            'name' => 'Enter FacadeName (ex. FileUpload)',
+        ];
     }
 }
--- tests/Unit/Commands/FacadeMakeCommandTest.php
+++ tests/Unit/Commands/FacadeMakeCommandTest.php
@@ -1,23 +1,79 @@
 <?php
 
-use Illuminate\Support\Facades\Artisan;
+use Orchestra\Testbench\Concerns\InteractsWithPublishedFiles;
 
-it('creates facade and service files', function () {
+uses(InteractsWithPublishedFiles::class);
 
-    $facade = app_path('Facades/FileUpload.php');
-    $service = app_path('Facades/Services/CommonFileUpload.php');
+beforeEach(function (): void {
+    $this->files = [
+        'app/Facades/FileUpload.php',
+        'app/Facades/Services/CommonFileUpload.php',
+        'app/Facades/TestFacade.php',
+        'app/Facades/Services/TestService.php',
+        'app/Facades/Foo/BarFacade.php',
+        'app/Facades/Services/Foo/BarService.php',
+    ];
+});
 
-    if (file_exists($facade)) unlink($facade);
-    if (file_exists($service)) unlink($service);
+it('can generate facade and service files', function (): void {
+    $this->artisan('make:facade', [
+        'name' => 'FileUpload',
+        'serviceClass' => 'CommonFileUpload',
+    ])->assertExitCode(0);
 
-    Artisan::call('make:facade', [
-        'facadeName' => 'FileUpload',
-        'facadeServiceClass' => 'CommonFileUpload',
-    ]);
+    $this->assertFileContains([
+        'namespace App\Facades;',
+        'use Illuminate\Support\Facades\Facade;',
+        'class FileUpload extends Facade',
+        'protected static function getFacadeAccessor(): mixed',
+        'return "commonfileupload";',
+    ], 'app/Facades/FileUpload.php');
 
-    expect($facade)->toBeFile();
-    expect($service)->toBeFile();
+    $this->assertFileContains([
+        'namespace App\Facades\Services;',
+        'class CommonFileUpload',
+        'public static function startToMakingFunction()',
+    ], 'app/Facades/Services/CommonFileUpload.php');
 
-    unlink($facade);
-    unlink($service);
+    $this->assertFilenameNotExists('app/Facades/TestFacade.php');
+    $this->assertFilenameNotExists('app/Facades/Services/TestService.php');
+});
+
+it('can generate facade with namespace', function (): void {
+    $this->artisan('make:facade', [
+        'name' => 'Foo\\BarFacade',
+        'serviceClass' => 'Foo\\BarService',
+    ])->assertExitCode(0);
+
+    $this->assertFileContains([
+        'namespace App\Facades\Foo;',
+        'use Illuminate\Support\Facades\Facade;',
+        'class BarFacade extends Facade',
+    ], 'app/Facades/Foo/BarFacade.php');
+
+    $this->assertFileContains([
+        'namespace App\Facades\Services\Foo;',
+        'class BarService',
+    ], 'app/Facades/Services/Foo/BarService.php');
+
+    $this->assertFilenameNotExists('app/Facades/FileUpload.php');
+    $this->assertFilenameNotExists('app/Facades/Services/CommonFileUpload.php');
+});
+
+it('prompts for service class when not provided', function (): void {
+    $this->artisan('make:facade', [
+        'name' => 'TestFacade',
+    ])
+        ->expectsQuestion('Enter FacadeServiceClass (ex. CommonFileUpload)', 'TestService')
+        ->assertExitCode(0);
+
+    $this->assertFileContains([
+        'namespace App\Facades;',
+        'class TestFacade extends Facade',
+    ], 'app/Facades/TestFacade.php');
+
+    $this->assertFileContains([
+        'namespace App\Facades\Services;',
+        'class TestService',
+    ], 'app/Facades/Services/TestService.php');
 });
